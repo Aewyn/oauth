78fe7d5d4d5d2226fb4a6b3609bbee64c6d52103
Aewyn <aewyn@outlook.com>
2022-08-18 11:12:30 +0200

    Initial commit

---
+++ src/main/java/com/example/oauth/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class security {
+    @Bean
+    public SecurityFilterChain geefRechten(HttpSecurity http) throws Exception{
+        http.oauth2Login(); //Definieer dat je OAuth gebruikt. De gebruiker zal dus inloggen op een andere website.
+        http.authorizeRequests(requests -> requests
+                .mvcMatchers("/").permitAll() //Iedereen heeft toegang tot de beginpagina
+                .mvcMatchers("/beveiligd").authenticated()); //Enkel ingelogde users hebben toegang tot de URL beveiligd, als ze deze pagina opnenen
+                                                                      //moeten ze eerst inloggen op GitHub.
+        return http.build();
+    }
+}
